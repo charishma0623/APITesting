90f5c37508a28a0812e1ec430835066871670558
charishma velu <charishma023@gmail.com>
2025-06-11 08:28:56 +0530

    initial commit

---
+++ API_testing/src/test/java/APItest/test1.java
@@ -0,0 +1,19 @@
+package APItest;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class test1 {
+	@Test
+	public void testex() {
+		Response res = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println(res.getStatusCode());
+		System.out.println(res.getTime());
+		System.out.println(res.getBody().asString());
+		System.out.println(res.getStatusLine());
+		System.out.println(res.getHeader("content-type"));
+	}
+
+}
